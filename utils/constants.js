c35992c9e0b2e1e96ed77a94ab87f7a2c0e21e08
MessaIina <novikovanadejhda@gmail.com>
2023-10-19 13:09:59 +0700

    1REW DIPLOMA

--- utils/constants.js
+++ utils/constants.js
@@ -1,14 +1,11 @@
 const OK = 200;
 const CREATED = 201;
 const BAD_REQUEST = 400;
-const UNAUTHORIZED = 401;
-const FORBIDDEN = 403;
-const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 
 const REG_EXP_LINK = /https?:\/\/(www\.)?[\w\-._~:/?#[\]@!\\$&'()\\*+,;=]+#?/;
 const REG_EXP_EMAIL = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-const REG_EXP_IMG = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{2,6}([-a-zA-Z0-9@:%_+.~#?&//=]*)\.(jpg|jpeg|png|gif|bmp)$/;
+const REG_EXP_IMG = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{2,6}([-a-zA-Z0-9@:%_+.~#?&//=]*)\.(jpg|jpeg|png|bmp)$/;
 const REG_EXP_VID = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{2,6}([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
 const REG_EXP_DATE = /^\d{4}$/;
 
@@ -23,9 +20,6 @@ module.exports = {
   OK,
   CREATED,
   BAD_REQUEST,
-  UNAUTHORIZED,
-  FORBIDDEN,
-  NOT_FOUND,
   INTERNAL_SERVER_ERROR,
   REG_EXP_LINK,
   REG_EXP_EMAIL,
